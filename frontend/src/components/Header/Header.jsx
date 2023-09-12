2ac91366cbf700ab12bc280e89624ad2730791bf
Hrishikesh Lamdade <hrishilamdade@gmail.com>
2023-09-13 01:33:31 +0530

    added .env for frontend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './Header.css'
-import config from '../../config.json'
 import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <div className='header'>
-        <div className='header__title' ><Link to="/"> {config.APP_NAME}</Link> </div>
+        <div className='header__title' ><Link to="/"> {process.env.APP_NAME}</Link> </div>
         <div>Profile</div>
     </div>
   )
